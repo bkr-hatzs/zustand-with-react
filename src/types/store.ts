ba77ee6a64d76be6d90c03984828366f92a528c8
binfateh340 <abubakarbinfateh@gmail.com>
2025-03-04 17:32:57 +0500

    little change in type

--- src/types/store.ts
+++ src/types/store.ts
@@ -2,3 +2,8 @@ import { CartSlice } from "@/store/cartSlice";
 import { UserSlice } from "@/store/userSlice";
 
 export type Store = UserSlice & CartSlice;
+
+export type abc = {
+    a: string;
+    b: number;
+}
