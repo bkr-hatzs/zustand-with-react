12de01892c0474e6f6b5981b2e9c58287036f536
binfateh340 <abubakarbinfateh@gmail.com>
2025-03-04 17:33:51 +0500

    fix

--- src/types/store.ts
+++ src/types/store.ts
@@ -2,8 +2,3 @@ import { CartSlice } from "@/store/cartSlice";
 import { UserSlice } from "@/store/userSlice";
 
 export type Store = UserSlice & CartSlice;
-
-export type abc = {
-    a: string;
-    b: number;
-}
